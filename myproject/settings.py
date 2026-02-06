a84688464f9bb69533e7a3e0bd0ba8f727dcd372
Jagannath Anshikar <jagannath@Jagannaths-MacBook-Air.local>
2026-02-06 12:11:51 +0530

    second

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
